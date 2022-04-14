472ae3092ff33200fec6ee1f46e8dfc41a536464
JeremyJMoss <mossyj19@gmail.com>
2022-04-14 12:48:10 +1000

    adjusted portfolio page

--- main.js
+++ main.js
@@ -7,42 +7,43 @@ const closeBtn = document.querySelector(".closeBtn");
 const typeContainer = document.querySelector(".typewriterContainer");
 const welcome = document.querySelector(".welcomeText");
 let writtenText = [
-  "Front-End Developer",
-  "Eager to Learn More",
-  "Lover of Vanilla CSS",
+	"Front-End Developer",
+	"Eager to Learn More",
+	"Python Enthusiast"
 ];
 let index = 0;
 let arrPos = 0;
 
 const typewriter = function () {
-  typeContainer.innerHTML = writtenText[arrPos].slice(0, index);
+	typeContainer.innerHTML = writtenText[arrPos].slice(0, index);
 
-  index++;
+	index++;
 
-  if (index > writtenText[arrPos].length) {
-    setTimeout(function () {
-      index = 0;
-      arrPos++;
-      if (arrPos > 2) arrPos = 0;
-    }, 1500);
-  }
+	if (index > writtenText[arrPos].length) {
+		setTimeout(function () {
+			index = 0;
+			arrPos++;
+			if (arrPos > 2) arrPos = 0;
+		}, 1500);
+	}
 };
 
 const dynamicWelcomeMessage = function () {
-  const today = new Date();
-  const currentHour = today.getHours();
-  if (currentHour > 0 && currentHour < 12) {
-    welcome.innerHTML = "Good Morning, ";
-  } else if (currentHour >= 12 && currentHour <= 5) {
-    welcome.innerHTML = "Good Day, ";
-  } else {
-    welcome.innerHTML = "Good Evening, ";
-  }
+	const today = new Date();
+	const currentHour = today.getHours();
+	console.log(currentHour);
+	if (currentHour => 5 && currentHour < 12) {
+		welcome.innerHTML = "Good Morning, ";
+	} else if (currentHour >= 12 && currentHour <= 17) {
+		welcome.innerHTML = "Good Day, ";
+	} else {
+		welcome.innerHTML = "Good Evening, ";
+	}
 };
 
 const openCloseMenu = function () {
-  blurBackground.classList.toggle("hide");
-  pageLinks.classList.toggle("hide");
+	blurBackground.classList.toggle("hide");
+	pageLinks.classList.toggle("hide");
 };
 
 hamburger.addEventListener("click", openCloseMenu);
